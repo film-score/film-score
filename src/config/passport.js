5a0a535c22c0cba937922a6edf1a40e2bb92eb7c
Michael Panik <michael@plexamedia.com>
2018-07-04 11:01:18 -0500

    Init Commit

---
+++ src/config/passport.js
@@ -0,0 +1,46 @@
+import passport from 'passport'
+import bcrypt from 'bcrypt'
+import db from '../db'
+
+passport.use(new LocalStrategy((username, password, cb) => {
+
+    db.query('SELECT id, username, password, type FROM users WHERE username=$1', [username], (err, result) => {
+        if(err) {
+            console.error('Error when selecting user on login', err)
+            return cb(err)
+        }
+    
+        if (result.rows.lenth > 0) {
+
+            const first = result.rows[0]
+
+            bcrypt.compare(password, first.password, function(err, res) {
+
+                if(res) {
+                    cb(null, { id: first.id, username: first.username, type: first.type })
+                } else {
+                    cb(null, false)
+                }
+
+            })
+        } else {
+            cb(null, false)
+        }
+    })
+
+}))
+
+passport.serializeUser((user, done) => {
+    done(null, user.id)
+})
+
+passport.deserializeUser((id, cb) => {
+    db.query('SELECT id, username, type FROM users WHERE id = $1', [parseInt(id, 10)], (err, res) => {
+        if(err) {
+            console.error('Error when selecting user on session deserialize', err)
+            return cb(err)
+        }
+
+        cb(null, results.rows[0])
+    })
+})
---
+++ src/db/index.js
@@ -0,0 +1,24 @@
+import pg from 'pg'
+import config from '../config'
+
+const dbConfig = {
+    user: config.db.user,
+    password: config.db.password,
+    database: config.db.database,
+    host: config.db.host,
+    port: config.db.port,
+    max: config.db.max,
+    idleTimeoutMillis: config.db.idleTimeoutMillis,
+}
+
+const pool = new pg.Pool(dbConfig)
+
+pool.on('error', function (err) {
+    winston.error('idle client error', err.message, err.stack)
+})
+
+module.exports = {
+    query: (text, params, callback) => {
+        return pool.query(text, params, callback)
+    }
+}
