62dbe27ae078ec639b60ce0662c3f4cb9baebb0a
Michael Panik <michael@plexamedia.com>
2018-07-30 16:53:34 -0500

    Make menu mobile responsive.

--- react-ui/src/components/Navbar/index.js
+++ react-ui/src/components/Navbar/index.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { Navbar, Nav, NavbarBrand, NavItem } from "reactstrap"
+import { Navbar, Nav, NavbarBrand, NavItem, Collapse, NavbarToggler } from "reactstrap"
 import { Link } from "@reach/router"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
@@ -26,22 +26,49 @@ const NavLink = props => (
     </NavItem>
 )
 
-const Navigation = (props) => {
-    return <Navbar color="dark" dark expand="lg" className="fixed-top">
-                
-                <NavbarBrand href="/">
-                    <img src="./logo.svg" alt="Film Score" width="150px"/>
-                </NavbarBrand>
-                
-                <Nav navbar className="mr-auto">
-                    <NavLink to="/" icon="list" label="Dashboard" />
-                    <NavLink to="/new-score" icon="star" label="New Score" />
-                    {props.currentUser.account_type === 'admin' ? <NavLink to="/admin" icon="cogs" label="Admin" /> : null}
-                </Nav>
-                
-            <div className="navbar-text">{props.currentUser.first_name + " " + props.currentUser.last_name}</div>
-                <button onClick={logout} className="btn btn-outline-danger ml-3">Logout</button>
-            </Navbar>
+class Navigation extends React.Component {
+    constructor(props) {
+        super(props)
+
+        this.toggle = this.toggle.bind(this)
+
+        this.state = {
+            isOpen: false
+        }
+    }
+
+    toggle() {
+        this.setState({
+            isOpen: !this.state.isOpen
+        })
+    }
+
+    render() {
+        return <Navbar color="dark" dark expand="md" className="fixed-top">
+                    
+                    <NavbarBrand href="/">
+                        <img src="./logo.svg" alt="Film Score" width="150px"/>
+                    </NavbarBrand>
+    
+                    <NavbarToggler onClick={this.toggle} />
+                    <Collapse isOpen={this.state.isOpen} navbar>
+                        
+                        <Nav navbar className="mr-auto">
+                            <NavLink to="/" icon="list" label="Dashboard" />
+                            <NavLink to="/new-score" icon="star" label="New Score" />
+                            {this.props.currentUser.account_type === 'admin' ? <NavLink to="/admin" icon="cogs" label="Admin" /> : null}
+                        </Nav>
+    
+                        <hr class="hidden-sm-up" />
+                        <div className="d-flex justify-content-between">
+                            <div className="navbar-text">{this.props.currentUser.first_name + " " + this.props.currentUser.last_name}</div>
+                            <button onClick={logout} className="btn btn-outline-danger ml-3">Logout</button>
+                        </div>
+    
+                    </Collapse>
+                    
+                </Navbar>
+    }
 }
 
 export default Navigation
